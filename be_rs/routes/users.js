0ae7fdb07a428688c99136109ca1971d8b7c8277
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2024-06-10 12:52:36 -0500

    feat: implementation of 'getAllUsers' in users.js in both routes and services

--- be_rs/routes/users.js
+++ be_rs/routes/users.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var userService = require("../services/users");
 
-/* GET users listing. */
+/** Por cada endpoint se realiza una función que luego será enrutada con router */
 
+/**
+ * Controlador para obtener todos los registros
+ * 
+ * @param {Object} req - el objeto de solicitud de Express
+ * @param {Object} res - el objeto de respuesta de Express
+ */
 var getAllUsersController = function (req, res, next) {
-  res.send('respond with a resource');
+  userService.getAllUsersServices()
+    .then((response) => {
+      res.json(response);
+    }).catch((error) => {
+      next(error);
+    });
 };
 
 var createUserController = function (req, res, next) {
@@ -23,6 +35,11 @@ var deleteUserByEmail = function (req, res, next) {
   res.send('respond with a resource');
 };
 
+/** 
+ * Rutas donde se realizará la operación, tener en cuenta:
+ * el verbo (get, post, put, delete) 
+ * el parametro '/:email...' cuando está especificado
+ * */
 router.get('/', getAllUsersController);
 router.post('/create', createUserController);
 router.get('/:email/detail', getUserByEmailController);
---
+++ be_rs/services/users.js
@@ -0,0 +1,33 @@
+var Promise = require("bluebird"); // importar bluebird para usar las funcionalidades de promesas
+var users = require("../mocks/users.json"); // importar el archivo con JSON de users para realizar pruebas
+
+/** 
+ * por cada función en router, se realizará una función asíncrona que devuelve una promesa 
+ * según la interaccion con los datos 
+ */
+
+/**
+ * obtiene todos los registros por medio del resolve
+ * 
+ * @returns - promesa según lo que se ejecute en el try-catch
+ * @param - no necesita parámetros 
+ * @method resolve - se ejecuta cuando la operacion es exitosa
+ * @method reject - se ejecuta cuando la operacion falló
+ * @description - try-catch para manejar correctamente los casos de éxito y de error,
+ * asegura que cualquier error será capturado y pasado al reject.
+ */
+var getAllUsersServices = async function () {
+    // ToDo: remove when the database implement
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(users);
+        } catch (error) {
+            reject(error);
+        }
+    })
+};
+
+// como este script no es visible, para exportar un módulo dentro de JS se usa el patrón factory
+module.exports = {
+    getAllUsersServices
+};
