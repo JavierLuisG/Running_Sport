bae9cd8dfbfc8cfa7e0cc67780912e633f270347
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2024-06-10 14:18:15 -0500

    feat: implementation of 'createUser' in users.js in both routes and services

--- be_rs/routes/users.js
+++ be_rs/routes/users.js
@@ -19,8 +19,24 @@ var getAllUsersController = function (req, res, next) {
     });
 };
 
+/**
+ * Controlador para crear un registro
+ * 
+ * @param {Object} req - el objeto de solicitud de Express.
+ * @param {Object} req.body - los datos del usuario a crear, proporcionados en el cuerpo de la solicitud.
+ * @param {Object} res - el objeto de respuesta de Express.
+ * @param {Function} next - la función middleware de Express para pasar el control al siguiente manejador.
+ * @description - si la creación es exitosa, responde con un estado 201 y los datos del nuevo usuario 
+ * y si ocurre un error, pasa el error al siguiente middleware.
+ */
 var createUserController = function (req, res, next) {
-  res.send('respond with a resource');
+  userService.createUserServices(req.body)
+    // ToDo: will change when the database is implemented
+    .then((response) => {
+      res.status(201).send(response);
+    }).catch((error) => {
+      next(error);
+    });
 };
 
 var getUserByEmailController = function (req, res, next) {
--- be_rs/services/users.js
+++ be_rs/services/users.js
@@ -27,7 +27,30 @@ var getAllUsersServices = async function () {
     })
 };
 
+/**
+ * crea un registro por medio del resolve 
+ * (todo: no se creará en puebas, se traerá un registro. Solo se estructura la función)
+ * 
+ * @returns - promesa según lo que se ejecute en el try-catch
+ * @param userParam - contiene los datos ingresados por el usuario 
+ * @method resolve - se ejecuta cuando la operacion es exitosa
+ * @method reject - se ejecuta cuando la operacion falló
+ * @description - try-catch para manejar correctamente los casos de éxito y de error,
+ * asegura que cualquier error será capturado y pasado al reject.
+ */
+var createUserServices = async function (userParam) {
+    // ToDo: remove when the database implement
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(users[0]);
+        } catch (error) {
+            reject(error);
+        }
+    })
+};
+
 // como este script no es visible, para exportar un módulo dentro de JS se usa el patrón factory
 module.exports = {
-    getAllUsersServices
+    getAllUsersServices,
+    createUserServices
 };
